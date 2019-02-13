c8f530edbaa6badf453f47b6127d4812d3b91f66
23michael45 <michael2345@live.cn>
2019-02-13 12:05:14 +0800

    add src

---
+++ DDRVersionTools/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDRVersionTools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args[0] == "compile-time")
+            {
+                string filename = args[1];
+                int linenum = Convert.ToInt32(args[2]);
+                VersionWriter vw = new VersionWriter();
+                vw.WriteTime(filename, linenum);
+            }
+        }
+    }
+}
---
+++ DDRVersionTools/VersionWriter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DDRVersionTools
+{
+    class VersionWriter
+    {
+        public void WriteTime(string filename,int linenum)
+        {
+            string[] file = File.ReadAllLines(filename);
+
+            for(int i = 0; i < file.Length;i++)
+            {
+                if(i == linenum)
+                {
+                    string line = file[i];
+                    DateTime dt = DateTime.Now;
+                    line = string.Format("std::string g_BuildTime = \"{0}\";", dt.ToString());
+                    file[i] = line;
+                }
+            }
+
+            File.WriteAllLines(filename, file);
+        }
+    }
+}
