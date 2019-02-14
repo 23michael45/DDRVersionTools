c0abcdd830f329a711900283367c3ef405045d60
23michael45 <michael2345@live.cn>
2019-02-14 12:03:14 +0800

    add download recent version file function

---
+++ DDRVersionTools/HttpDownloader.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DDRVersionTools
+{
+    class HttpDownloader
+    {
+        bool bComplete = false;
+
+        //string basePath = "http://111.230.250.213:8000/Distribution/";    mode->"Debug or Release"
+        public void DownloadRecent(string basePath,string filename,string mode = "Debug")
+        {
+            string verPath = GetVersionPath(basePath,mode);
+            string url = basePath + verPath + "/" + filename;
+
+            DownloadFile(url, filename);
+        }
+
+
+        //http://111.230.250.213:8000/Distribution/Debug.txt
+        public string GetVersionPath(string basePath,string mode)
+        {
+            using (var client = new WebClient())
+            {
+                string path = client.DownloadString(basePath + mode + @".txt");
+                return path;
+            }
+        }
+    
+
+
+
+        public void DownloadFile(string url,string filename)
+        {
+            using (var client = new WebClient())
+            {
+                Console.WriteLine(string.Format("Downloading: {0}", url));
+
+                client.DownloadProgressChanged += OnDownloadProgressChanged;
+                client.DownloadFileCompleted += OnDownloadFileCompleted;
+                client.DownloadFileAsync(new Uri(url), filename);
+
+                while(!bComplete)
+                {
+                    Thread.Sleep(1000);
+                }
+            }
+
+        }
+
+        void OnDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+
+
+
+            double bytesIn = double.Parse(e.BytesReceived.ToString());
+            double totalBytes = double.Parse(e.TotalBytesToReceive.ToString());
+
+            double percentage = bytesIn / totalBytes * 100;
+
+            Console.WriteLine(string.Format("Downloaded {0} of {1}  Progress: {2}", e.BytesReceived,e.TotalBytesToReceive, percentage));
+        }
+        public void OnDownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            bComplete = true;
+            Console.WriteLine(string.Format("Downloading Complete"));
+        }
+    }
+}
--- DDRVersionTools/Program.cs
+++ DDRVersionTools/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,13 +12,46 @@ namespace DDRVersionTools
     {
         static void Main(string[] args)
         {
-            if(args[0] == "compile-time")
+            if (args[0] == "compile-time")
             {
-                string filename = args[1];
-                int linenum = Convert.ToInt32(args[2]);
-                VersionWriter vw = new VersionWriter();
-                vw.WriteTime(filename, linenum);
+                CompileTime(args);
             }
+            else if (args[0] == "download-recent")
+            {
+                DownloadRecent(args);
+            }
+            else if (args[0] == "download-file")
+            {
+                DownloadRecent(args);
+            }
+        }
+
+        //DDRVersionTools.exe compile-time Version.h 2
+        static void CompileTime(string[] args)
+        {
+            string filename = args[1];
+            int linenum = Convert.ToInt32(args[2]);
+            VersionWriter vw = new VersionWriter();
+            vw.WriteTime(filename, linenum);
+
+        }
+
+        //DDRVersionTools download-recent http://111.230.250.213:8000/Distribution/ DDR_LocalServer.exe Debug
+        static void DownloadRecent(string[] args)
+        {
+            string url = args[1];
+            string filename = args[2];
+            string mode = args[3];
+            HttpDownloader httpDownloader = new HttpDownloader();
+            httpDownloader.DownloadRecent(url, filename,mode);
+        }
+        //DDRVersionTools download-file http://111.230.250.213:8000/Distribution/Debug/2019-2-13/DDR_LocalServer.exe
+        static void DownloadFile(string[] args)
+        {
+            string url = args[1];
+            string filename = Path.GetFileName(url);
+            HttpDownloader httpDownloader = new HttpDownloader();
+            httpDownloader.DownloadFile(url, filename);
         }
     }
 }
